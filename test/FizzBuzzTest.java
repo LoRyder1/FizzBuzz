020978610bf59b2172f45dedacb3509c37b9231a
Lovinder Pnag <lspnag@gmail.com>
2016-02-29 13:32:13 -0500

    refacored FizzBuzz tests

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -1,23 +1,29 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void whenFizzBuzzIsPassedANumberItReturnsThatNumber() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.fizz(1));
     }
 
     @Test
     public void whenFizzBuzzIsPassedATwoItReturnsATwo() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.fizz(2));
     }
 
     @Test
     public void whenFizzBuzzisPassedAMultipleOfThreeItReturnsFizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("fizz", fizzBuzz.fizz(3));
         assertEquals("fizz", fizzBuzz.fizz(6));
     }
